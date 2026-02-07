f159a95eb314f43e1b3f9203df8b73cbe4e3e6ac
Rocco Taccogna <roccotaccogna@hotmail.com>
2026-02-07 17:19:47 +0100

    update

--- src/server.js
+++ src/server.js
@@ -14,7 +14,10 @@ app.use(express.json());
 
 // Autorizzazione CORS
 app.use(cors({ 
-  origin: 'http://localhost:5173', 
+  origin: [
+    'http://localhost:5173', 
+    'https://myprods-frontend.vercel.app'
+  ],
   methods: 'GET,POST,PUT,DELETE', 
   allowedHeaders: 'Content-Type,Authorization' 
 }));
